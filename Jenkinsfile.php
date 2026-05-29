fb9e9b9f6e4f1ebf73cc5d75793d32dc003ec5d1
kavyakr9599 <kavyakr9599@gmail.com>
2026-05-29 07:53:27 +0530

    new commit

---
+++ Jenkinsfile.php
@@ -0,0 +1,52 @@
+pipiline {
+    agent any 
+     
+    tools {
+        nodejs "node18"
+    }
+    environment {
+        AWS_REGION = 'us-east-1'
+        S3_BUCKET = 'static-website-bucket-29'
+        BUILD_DIR = 'dist'
+    }
+    stages {
+        stage('Checkout') {
+            steps {
+                git branch: 'main',
+                url: 'https://github.com/kavyakr9599-dotcom/static-web.git'
+            }
+        }
+         stage('Build') {
+            steps {
+                echo "installing dependencies"
+            
+                sh 'npm install'
+                
+                echo "building static website"
+                 
+                sh 'npm run build'
+                 
+               echo "archiving build artifacts"
+               archiveArtifacts artifacts: 'dist/**'
+            }
+         }
+          
+         stage(deploy on s3) {
+            steps {
+                echo "Deploying application to s3"
+
+                sh """
+                aws s3 sync ${BUILD_DIR}/ s3://${S3_BUCKET} --delete
+                """
+            }
+         }
+    }
+    post {
+        sccess {
+            echo "deployment completed successfully"
+        }
+        failure {
+            echo "pipeline failed"
+        }
+    }
+}
